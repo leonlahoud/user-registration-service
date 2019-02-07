3f30a8e6b8d7b2aa1e6d1470be74ca14d9329922
Leon Lahoud <leonlahoud@yahoo.com>
2019-02-06 19:07:18 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {UserRegistrationServiceApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {UserRegistrationServiceApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new UserRegistrationServiceApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
